69fe938c0752fae1941566259ea61ed50fce42a5
syncmaster851 <syncmaster851@gmail.com>
2018-01-23 21:45:37 +0800

    refactor the functions name.

--- score.py
+++ score.py
@@ -8,20 +8,20 @@ logging.getLogger().setLevel(logging.INFO)
 names=[]
 scores={}
 
-def add_to_dict(name):
+def generate_scores(name):
     scores[name]= {"language and literature":random.randint(1,100), "math": random.randint(1, 100), "english": random.randint(1, 100)}
 
-def add_to_list(number):
+def generate_students(number):
     numbers=str(number)+"#"
     names.append(numbers)
 
 logging.info("Studentnumber\tLanguage and literature\tMath\tEnglish")
 
 for looper in range(1, 37):
-    add_to_list(looper)
+    generate_students(looper)
 
 for studentnumber in names:
-    add_to_dict(studentnumber)
+    generate_scores(studentnumber)
 
 for i in names:
     ll = scores[i]['language and literature']
